577913a227c2b5bcf2891b7254ac0dcb9eaf2012
Aditya Ahirrao <adityaahirrao4503@gmail.com>
2024-02-07 21:50:10 +0530

    Add files via upload

---
+++ threesquares.py
@@ -0,0 +1,11 @@
+#Legendre's three-square theorem    :      n=4**a(8*b+7)
+def threesquares(n): 
+    condition = True
+    for i in range(0,n+1):
+        for j in range(0,n+1):
+            if n==(4**i)*(8*j+7):
+                condition = False
+                break
+    print(condition)
+    
+threesquares(143)
